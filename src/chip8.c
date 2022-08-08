ca320501ff61d1a755075b37e71b52c80303fea2
Pedro F. Carvalho <pedrofrca@gmail.com>
2022-08-07 23:48:55 -0300

    Finished all instructions. Testing needed.

--- src/chip8.c
+++ src/chip8.c
@@ -4,9 +4,13 @@
 #define RAM_SIZE 4096
 #define RAM_PROGRAM_START 512
 #define MAX_GAME_SIZE 4096-512
+#define SHIFT_INSTRUCTION 1 //if 1, 8XY6 and 8XYE just shift vx. if 0 first sets vx to vy then shifts
+#define JUMP_INSTRUCTION 1 //if 1, BNNN uses v0, else it becomes BXNN, using vx
+#define STORE_INSTRUCTION 1 //if 1, does not increment index while storing/loading registers
 
 #include <stdio.h>
-//#include <time.h>
+#include <stdlib.h>
+#include <time.h>
 #include <SDL2/SDL.h>
 
 typedef struct { 
@@ -21,6 +25,7 @@ typedef struct {
   unsigned short subroutine_stack [16]; //contains information to return from subroutines
   unsigned short SP; //points to top of subroutine stack
   unsigned char key; //current key being pressed on keypad
+  unsigned char was_key_pressed; //variable that stores if there is currently a key being pressed
 } Chip8;
 
 //SDL global variables
@@ -32,6 +37,9 @@ SDL_Texture *texture = NULL;
 //input: chip8 struct
 void Chip8_init(Chip8 *chip8){
   int i;
+
+  //starts random number
+  srand(time(NULL));
 
   //clearing RAM
   for (i = 0; i < 4096; i++)
@@ -78,10 +86,11 @@ void Chip8_init(Chip8 *chip8){
   chip8->SP = 0;
   chip8->opcode = 0;
   chip8->key = 0;
+  chip8->was_key_pressed = 0;
   chip8->delay_timer = 0;
   chip8->sound_timer = 0;
 
-  //initializing sSDL
+  //initializing SDL
   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
       printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
       return;
@@ -148,14 +157,64 @@ void instr_clearScreen(Chip8 *chip8) {
   Chip8_drawDisplay(chip8);
 }
 
-//1NNN: jump to instruction in address NNN
+//00EE: returns from subroutine
+void instr_return(Chip8 *chip8) {
+  chip8->PC = chip8->subroutine_stack[chip8->SP];
+  chip8->SP -= 1;
+}
+
+//1NNN: jump to instruction in address nnn
 void instr_jump(Chip8 *chip8) {
   unsigned short nnn = chip8->opcode & 0x0FFF;
   chip8->PC = nnn;
 }
 
-//6XNN: set register VX to the value NN
-void instr_setVX(Chip8 *chip8) {
+//2NNN: call subroutine in address nnn
+void instr_callSubroutine(Chip8 *chip8) {
+  unsigned short nnn = chip8->opcode & 0x0FFF;
+  
+  chip8->SP += 1;
+  chip8->PC -= 2;
+  chip8->subroutine_stack[chip8->SP] = chip8->PC;
+  chip8->PC = nnn;
+}
+
+//3XNN: skips next instruction if vx = nn
+void instr_skipEq_vx_nn(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short nn = chip8->opcode & 0x00FF;
+
+  if (chip8->V[x] == nn) {
+    chip8->PC += 2;
+  }
+}
+
+//4XNN: skips next instruction if vx != nn
+void instr_skipNEq_vx_nn(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short nn = chip8->opcode & 0x00FF;
+
+  if (chip8->V[x] != nn) {
+    chip8->PC += 2;
+  }
+}
+
+//5XY0: skips next instruction if vx == vy
+void instr_skipEq_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  if (chip8->V[x] == chip8->V[y]) {
+    chip8->PC += 2;
+  }
+}
+
+//6XNN: set register vx to the value nn
+void instr_set_vx_nn(Chip8 *chip8) {
   unsigned short x = chip8->opcode & 0x0F00;
   x = x >> 8;
   unsigned short nn = chip8->opcode & 0x00FF;
@@ -163,8 +222,8 @@ void instr_setVX(Chip8 *chip8) {
   chip8->V[x] = nn;
 }
 
-//7XNN: add value NN to register VX
-void instr_addValVX(Chip8 *chip8) {
+//7XNN: add value nn to vx
+void instr_add_vx_nn(Chip8 *chip8) {
   unsigned short x = chip8->opcode & 0x0F00;
   x = x >> 8;
   unsigned short nn = chip8->opcode & 0x00FF;
@@ -172,13 +231,184 @@ void instr_addValVX(Chip8 *chip8) {
   chip8->V[x] = chip8->V[x] + nn;
 }
 
-//ANNN: set index to value NNN
-void instr_setI(Chip8 *chip8) {
+//8XY0: sets vx to value of vy
+void instr_set_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  chip8->V[x] = chip8->V[y];
+}
+
+//8XY1: vx gets the result of vx OR vy (bitwise operation)
+void instr_or_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  chip8->V[x] = chip8->V[x] | chip8->V[y];
+}
+
+//8XY2: vx gets the result of vx AND vy (bitwise operation)
+void instr_and_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  chip8->V[x] = chip8->V[x] & chip8->V[y];
+}
+
+//8XY3: vx gets the result of vx XOR vy (bitwise operation)
+void instr_xor_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  chip8->V[x] = chip8->V[x] ^ chip8->V[y];
+}
+
+//8XY4: vx gets the result of vx + vy
+//vf = 1 if carry
+void instr_add_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  unsigned short sum;
+
+  sum = chip8->V[x] + chip8->V[y];
+  chip8->V[x] = sum;
+
+  if (sum > 255)
+    chip8->V[0xF] = 1;
+}
+
+//8XY5: vx gets the result of vx - vy
+//vf = 0 if borrows
+void instr_sub_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  unsigned short sub;
+
+  sub = chip8->V[x] - chip8->V[y];
+  chip8->V[x] = sub;
+
+  if (sub > 0)
+    chip8->V[0xF] = 1;
+  else
+    chip8->V[0xF] = 0;
+}
+
+//8XY6: sets vx to value of vy then shifts vx to the right
+//or just shifts vx to the right (see SHIFT_INSTRUCTION)
+//vf gets the shifted bit
+void instr_shr_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  if (SHIFT_INSTRUCTION == 0)
+    chip8->V[x] = chip8->V[y];
+  
+  if (chip8->V[x] & 0x01 == 1)
+    chip8->V[0xF] = 1;
+  else
+    chip8->V[0xF] = 0;
+  
+  chip8->V[x] = chip8->V[x] >> 1;
+}
+
+//8XY7: vx gets the result of vy - vx
+//vf = 0 if borrows
+void instr_sub_vy_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  unsigned short sub;
+
+  sub = chip8->V[y] - chip8->V[x];
+  chip8->V[x] = sub;
+
+  if (sub > 0)
+    chip8->V[0xF] = 1;
+  else
+    chip8->V[0xF] = 0;
+}
+
+//8XY6: sets vx to value of vy then shifts vx to the left
+//or just shifts vx to the left (see SHIFT_INSTRUCTION)
+//vf gets the shifted bit
+void instr_shl_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  if (SHIFT_INSTRUCTION == 0)
+    chip8->V[x] = chip8->V[y];
+  
+  if (chip8->V[x] & 0x80 == 0x80)
+    chip8->V[0xF] = 1;
+  else
+    chip8->V[0xF] = 0;
+  
+  chip8->V[x] = chip8->V[x] << 1;
+}
+
+//9XY0: skips next instruction if vx != vy
+void instr_skipNEq_vx_vy(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short y = chip8->opcode & 0x00F0;
+  y = y >> 4;
+
+  if (chip8->V[x] != chip8->V[y]) {
+    chip8->PC += 2;
+  }
+}
+
+//ANNN: set index to value nnn
+void instr_set_i(Chip8 *chip8) {
   unsigned short nnn = chip8->opcode & 0x0FFF;
   chip8->I = nnn; 
 }
 
+//BNNN: jump with offset
+void instr_jumpOffset(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned short nn = chip8->opcode & 0x00FF;
+  unsigned short nnn = chip8->opcode & 0x0FFF;
+
+  if (JUMP_INSTRUCTION == 1)
+    chip8->PC = nnn + chip8->V[0];
+  else
+    chip8->PC = nn + chip8->V[x];
+}
+
+//CXNN: generates a random number then AND with nn
+void instr_rand(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned char nn = chip8->opcode & 0x00FF;
+
+  unsigned char rn = rand();
+  chip8->V[x] = rn & nn;
+}
+
 //DXYN: draw N pixels tall sprite from memory pointed by the index starting in coordinate (x, y)
+//vf = 1 if there is collision
 void instr_draw(Chip8 *chip8) {
   unsigned short x = chip8->opcode & 0x0F00;
   x = x >> 8;
@@ -213,6 +443,124 @@ void instr_draw(Chip8 *chip8) {
   Chip8_drawDisplay(chip8);
 }
 
+//EX9E: skips instruction if key corresponding to vx is pressed
+void instr_skipEq_vx_key(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  if (chip8->V[x] == chip8->key) {
+    chip8->PC += 2;
+  }
+}
+
+//EXA1: skips instruction if key corresponding to vx is not pressed
+void instr_skipEq_vx_key(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  if (chip8->V[x] != chip8->key) {
+    chip8->PC += 2;
+  }
+}
+
+//FX07: vx gets current delay timer
+void instr_set_vx_delayTimer(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  chip8->V[x] = chip8->delay_timer;
+}
+
+//FX0A: blocks execution while waiting for key and stores in vx when inputed
+void instr_getKey(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  if (chip8->was_key_pressed) {
+    chip8->V[x] = chip8->key;
+    chip8->was_key_pressed = 0;
+  }
+  else
+    chip8->PC -= 2;
+}
+
+//FX15: delay timer gets vx
+void instr_set_delayTimer_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  chip8->delay_timer = chip8->V[x];
+}
+
+//FX18: sound timer gets vx
+void instr_set_delayTimer_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  chip8->sound_timer = chip8->V[x];
+}
+
+//FX1E: add to index
+//vf gets 1 if i exceeds adressing range (0x0FFF)
+void instr_add_i_x(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+
+  chip8->I =  chip8->I + chip8->V[x];
+
+  if (chip8->I > 0x0FFF)
+    chip8->V[0xF] = 1;
+}
+
+//FX29: points i to hex char in last nibble of vx
+void instr_add_i_x(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned char vx = chip8->V[x] & 0x0F;
+
+  chip8->I = vx * 5;
+}
+
+//FX33: stores BCD value in vx in ram
+void instr_store_vx_bcd(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  unsigned char vx = chip8->V[x];
+
+  unsigned char dec100 = vx / 100;
+  unsigned char dec10 = (vx / 10) % 10;
+  unsigned char dec1 = (vx % 100) % 10;
+
+  chip8->ram[chip8->I] = dec100;
+  chip8->ram[(chip8->I) + 1] = dec10;
+  chip8->ram[(chip8->I) + 2] = dec1;
+
+}
+
+//FX55: stores from v0 to vx in ram
+void instr_store_v0_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  
+  int i;
+  for (i = 0; i <= x; i++)
+    chip8->ram[chip8->I + i] = chip8->V[i];
+  if (STORE_INSTRUCTION == 0)
+    chip8->I = chip8 I + x + 1;
+}
+
+//FX55: load in v0 to vx ram data
+void instr_store_v0_vx(Chip8 *chip8) {
+  unsigned short x = chip8->opcode & 0x0F00;
+  x = x >> 8;
+  
+  int i;
+  for (i = 0; i <= x; i++)
+    chip8->V[i] = chip8->ram[chip8->I + i];
+  if (STORE_INSTRUCTION == 0)
+    chip8->I = chip8 I + x + 1;
+}
+
 //implementation of the instruction fetch -> decode -> execute loop of the chip 8 interpreter
 //input: initialized chip8 struct 
 void Chip8_interpreterMainLoop(Chip8 *chip8) {
@@ -227,7 +575,7 @@ void Chip8_interpreterMainLoop(Chip8 *chip8) {
     unsigned short opcode_nibble4 = chip8->opcode & 0x000F;
     unsigned short opcode_byte2 = chip8->opcode & 0x00FF;
 
-    chip8->PC = chip8->PC + 2;
+    chip8->PC += 2;
 
     //decode & execute stage
     printf("addr: %#04X, opcode: %#04X, instruction: ", chip8->PC, chip8->opcode);
@@ -272,12 +620,12 @@ void Chip8_interpreterMainLoop(Chip8 *chip8) {
 
       case 0x6000: //6XNN assign (val to reg)
         printf("6XNN");
-        instr_setVX(chip8);
+        instr_set_vx_nn(chip8);
       break;
 
       case 0x7000: //7XNN accumulate (val to reg)
         printf("7XNN");
-        instr_addValVX(chip8);
+        instr_add_vx_nn(chip8);
       break;
       
       case 0x8000:
@@ -330,7 +678,7 @@ void Chip8_interpreterMainLoop(Chip8 *chip8) {
 
       case 0xA000: //ANNN assign to ram pointer
         printf("ANNN");
-        instr_setI(chip8);
+        instr_set_i(chip8);
       break;
 
       case 0xB000: //BNNN jump to addr + v0
